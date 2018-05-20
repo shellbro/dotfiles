1069d24ed02775250fc408611813641d18d7119c
Jakub Gorczyca <shellbro@users.noreply.github.com>
2018-05-20 13:46:54 +0200

    Add HideShow support to nXML major mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,18 +32,35 @@
 
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
-;; WS
+;; WS (built-in)
 (setq whitespace-style '(face tabs empty trailing lines-tail))
 (global-whitespace-mode t)
 
-;; hs
+;; hs (build-in)
 (add-hook 'prog-mode-hook
-	  (lambda()
-	    (local-set-key (kbd "C-c <right>") 'hs-show-block)
-	    (local-set-key (kbd "C-c <left>") 'hs-hide-block)
-	    (local-set-key (kbd "C-c <up>") 'hs-hide-all)
-	    (local-set-key (kbd "C-c <down>") 'hs-show-all)
-	    (hs-minor-mode t)))
+          (lambda()
+            (local-set-key (kbd "C-c <right>") 'hs-show-block)
+            (local-set-key (kbd "C-c <left>") 'hs-hide-block)
+            (local-set-key (kbd "C-c <up>") 'hs-hide-all)
+            (local-set-key (kbd "C-c <down>") 'hs-show-all)
+            (hs-minor-mode t)))
+;; FIXME:
+(require 'sgml-mode)
+(add-to-list 'hs-special-modes-alist
+             '(nxml-mode
+               "<!--\\|<[^/>]*[^/]>"
+               "-->\\|</[^/>]*[^/]>"
+
+               "<!--"
+               sgml-skip-tag-forward
+               nil))
+(add-hook 'nxml-mode-hook
+          (lambda()
+            (local-set-key (kbd "C-c <right>") 'hs-show-block)
+            (local-set-key (kbd "C-c <left>") 'hs-hide-block)
+            (local-set-key (kbd "C-c <up>") 'hs-hide-all)
+            (local-set-key (kbd "C-c <down>") 'hs-show-all)
+            (hs-minor-mode t)))
 
 ;; company
 (add-hook 'after-init-hook 'global-company-mode)
@@ -60,14 +77,14 @@
 (add-hook 'prog-mode-hook #'smartparens-mode)
 (add-hook 'cider-repl-mode-hook #'smartparens-mode)
 
-;; -
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-(add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
-
 ;; =>
 (add-hook 'clojure-mode-hook #'aggressive-indent-mode)
 (add-hook 'cider-repl-mode-hook #'aggressive-indent-mode)
 (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+
+;; -
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'cider-repl-mode-hook #'rainbow-delimiters-mode)
 
 (load "server")
 (unless (server-running-p)
@@ -81,7 +98,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (solarized-theme company magit markdown-mode yaml-mode cider aggressive-indent rainbow-delimiters smartparens))))
+    (rainbow-delimiters aggressive-indent smartparens cider yaml-mode markdown-mode magit company solarized-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
